b1db0a64db55340575696252a8ff34c7451e267a
Yunseok Kang <yunseok253@gmail.com>
2020-01-12 10:36:43 +0900

    Mvvm 페이지 테스트 추가

---
+++ TicTacToeExample.Tests/View/MvvmTests.cs
@@ -0,0 +1,18 @@
+﻿using TicTacToeExample.View;
+using Xunit;
+
+namespace TicTacToeExample.Test.View
+{
+    public class MvvmTests
+    {
+        private Mvvm view;
+
+        [WpfFact]
+        public void CreateTest()
+        {
+            view = new Mvvm();
+
+            Assert.NotNull(view);
+        }
+    }
+}
